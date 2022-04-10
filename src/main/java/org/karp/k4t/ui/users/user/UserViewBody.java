73a85e12b0546541b64aa78a2d2a24f6a4e1f4cc
ofer.karp <ofer.karp@gmail.com>
2022-04-10 20:16:17 +0300

    initial setup

--- src/main/java/org/karp/k4t/ui/users/user/UserViewBody.java
+++ src/main/java/org/karp/k4t/ui/users/user/UserViewBody.java
@@ -1,34 +1,40 @@
 package org.karp.k4t.ui.users.user;
 
 import com.vaadin.flow.component.dependency.CssImport;
-import com.vaadin.flow.component.html.H2;
-import com.vaadin.flow.component.orderedlayout.HorizontalLayout;
+import com.vaadin.flow.component.html.Label;
+import com.vaadin.flow.component.orderedlayout.FlexComponent;
+import com.vaadin.flow.component.orderedlayout.VerticalLayout;
+import com.vaadin.flow.router.RouterLink;
 import org.karp.k4t.ui.users.UsersDataProvider;
+import org.karp.k4t.ui.users.UsersView;
 
-import static com.vaadin.flow.component.orderedlayout.FlexComponent.Alignment.CENTER;
 import static org.karp.k4t.ui.Styles.CSS_FILE_EXTENSION;
 import static org.karp.k4t.ui.users.user.UserView.USER_FOLDER;
 import static org.karp.k4t.ui.users.user.UserViewBody.ID_SUFFIX;
 
 @CssImport(USER_FOLDER + ID_SUFFIX + CSS_FILE_EXTENSION)
-public class UserViewBody extends HorizontalLayout {
+public class UserViewBody extends VerticalLayout {
 
     public static final String ID_SUFFIX = "user-view-body";
 
-    private final H2 label;
+    private final Label selectedIdLabel;
+    private final RouterLink usersRouterLink;
 
     public UserViewBody(UsersDataProvider usersViewDataProvider) {
         setId(ID_SUFFIX);
         addClassName(ID_SUFFIX);
 
-        setWidthFull();
-        setAlignItems(CENTER);
+        setHeightFull();
+        setAlignItems(FlexComponent.Alignment.CENTER);
 
-        label = new H2("say what again");
-        add(label);
+        selectedIdLabel = new Label();
+        add(selectedIdLabel);
+
+        usersRouterLink = new RouterLink(UsersView.USERS_VIEW_NAME, UsersView.class);
+        add(usersRouterLink);
     }
 
     public void selectedUserChanged(long selectedUserId) {
-        label.setText("Selected User: " + selectedUserId);
+        selectedIdLabel.setText("Selected User: " + selectedUserId);
     }
 }
