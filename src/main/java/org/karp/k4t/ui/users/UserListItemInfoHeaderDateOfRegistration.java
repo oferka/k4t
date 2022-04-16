d668c331fa4340da254148c5336ae556fd9f6a63
ofer.karp <ofer.karp@gmail.com>
2022-04-16 13:12:42 +0300

    initial setup

--- src/main/java/org/karp/k4t/ui/users/UserListItemInfoHeaderDateOfRegistration.java
+++ src/main/java/org/karp/k4t/ui/users/UserListItemInfoHeaderDateOfRegistration.java
@@ -4,6 +4,9 @@ import com.vaadin.flow.component.dependency.CssImport;
 import com.vaadin.flow.component.html.Span;
 import org.karp.k4t.model.User;
 
+import java.time.format.DateTimeFormatter;
+
+import static java.time.format.FormatStyle.MEDIUM;
 import static org.karp.k4t.ui.Styles.CSS_FILE_EXTENSION;
 import static org.karp.k4t.ui.users.UserListItemInfoHeaderDateOfRegistration.ID;
 import static org.karp.k4t.ui.users.UsersView.FOLDER;
@@ -15,6 +18,6 @@ public class UserListItemInfoHeaderDateOfRegistration extends Span {
 
     public UserListItemInfoHeaderDateOfRegistration(User user) {
         addClassNames(ID);
-        setText(user.getDateOfRegistration().toString());
+        setText(user.getDateOfRegistration().format(DateTimeFormatter.ofLocalizedDate(MEDIUM)));
     }
 }
