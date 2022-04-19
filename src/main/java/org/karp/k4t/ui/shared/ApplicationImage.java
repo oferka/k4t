3bea4a86a498b03dc41386bb6a5d2a8fd380438d
ofer.karp <ofer.karp@gmail.com>
2022-04-19 17:44:42 +0300

    initial setup

--- src/main/java/org/karp/k4t/ui/shared/ApplicationImage.java
+++ src/main/java/org/karp/k4t/ui/shared/ApplicationImage.java
@@ -1,8 +1,13 @@
 package org.karp.k4t.ui.shared;
 
+import com.vaadin.flow.component.ClickEvent;
 import com.vaadin.flow.component.dependency.CssImport;
 import com.vaadin.flow.component.html.Image;
+import com.vaadin.flow.component.notification.Notification;
 
+import static com.vaadin.flow.component.Key.KEY_K;
+import static com.vaadin.flow.component.KeyModifier.ALT;
+import static com.vaadin.flow.component.notification.Notification.Position.MIDDLE;
 import static org.karp.k4t.Application.APPLICATION_LOGO;
 import static org.karp.k4t.Application.APPLICATION_NAME;
 import static org.karp.k4t.ui.Styles.CSS_FILE_EXTENSION;
@@ -17,5 +22,11 @@ public class ApplicationImage extends Image {
         addClassName(ID);
         setSrc(APPLICATION_LOGO);
         setAlt(APPLICATION_NAME);
+        addClickListener(this::applicationImageClicked);
+        addClickShortcut(KEY_K, ALT);
+    }
+
+    private void applicationImageClicked(ClickEvent<Image> event) {
+        Notification.show("Application Image Clicked", 3000, MIDDLE);
     }
 }
