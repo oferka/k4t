f115d676d302ca56c541247e50e39837dddfe1e5
ofer.karp <ofer.karp@gmail.com>
2022-04-24 15:17:22 +0300

    initial setup

---
+++ src/main/java/org/karp/k4t/controller/UserController.java
@@ -0,0 +1,147 @@
+package org.karp.k4t.controller;
+
+import io.micrometer.core.annotation.Counted;
+import io.micrometer.core.annotation.Timed;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.media.ArraySchema;
+import io.swagger.v3.oas.annotations.media.Content;
+import io.swagger.v3.oas.annotations.media.Schema;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.responses.ApiResponses;
+import lombok.RequiredArgsConstructor;
+import org.karp.k4t.model.User;
+import org.karp.k4t.service.UserService;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import javax.validation.constraints.NotNull;
+import java.util.List;
+
+import static org.karp.k4t.integration.Paths.USER_PATH;
+import static org.springframework.http.MediaType.APPLICATION_JSON_VALUE;
+
+@RestController
+@RequiredArgsConstructor
+@RequestMapping(path = USER_PATH)
+public class UserController {
+
+    private final UserService userService;
+
+    @GetMapping
+    @Timed(value = "UserController.findAll.timer", description = "Timer for user findAll endpoint", percentiles = { 0.01, 0.05,0.50, 0.95, 0.99})
+    @Counted(value = "UserController.findAll.counter", description = "Counter for user findAll endpoint")
+    @Operation(summary = "Find all users")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "200", description = "Users successfully found", content = { @Content(mediaType = APPLICATION_JSON_VALUE, array = @ArraySchema(schema = @Schema(implementation = User.class)))}),
+            @ApiResponse(responseCode = "400", description = "Failed to find users", content = @Content) }
+    )
+    public @NotNull ResponseEntity<List<User>> findAll() {
+        List<User> items = userService.findAll();
+        return ResponseEntity.ok(items);
+    }
+
+//    @GetMapping(value = "{id}")
+//    @Timed(value = "SearchTermController.findById.timer", description = "Timer for search term findById endpoint", percentiles = { 0.01, 0.05,0.50, 0.95, 0.99})
+//    @Counted(value = "SearchTermController.findById.counter", description = "Counter for search term findById endpoint")
+//    @Operation(summary = "Find a search term by id")
+//    @ApiResponses(value = {
+//            @ApiResponse(responseCode = "200", description = "Search term successfully found by id", content = { @Content(mediaType = APPLICATION_JSON_VALUE, schema = @Schema(implementation = SearchTerm.class))}),
+//            @ApiResponse(responseCode = "404", description = "Search term with specified id was not found", content = @Content),
+//            @ApiResponse(responseCode = "400", description = "Failed to find search term by id", content = @Content) })
+//    public @NotNull ResponseEntity<SearchTerm> findById(@Parameter(description = "The id of the search term to be found") @PathVariable("id") @NotNull Long id) {
+//        Optional<SearchTerm> item = searchTermService.findById(id);
+//        return item.map(ResponseEntity::ok).orElseGet(() -> ResponseEntity.notFound().build());
+//    }
+//
+//    @GetMapping(path = RANDOM_PATH)
+//    @Timed(value = "SearchTermController.findRandom.timer", description = "Timer for search term findRandom endpoint", percentiles = { 0.01, 0.05,0.50, 0.95, 0.99})
+//    @Counted(value = "SearchTermController.findRandom.counter", description = "Counter for search term findRandom endpoint")
+//    @Operation(summary = "Find a random search term")
+//    @ApiResponses(value = {
+//            @ApiResponse(responseCode = "200", description = "Random search term successfully found", content = { @Content(mediaType = APPLICATION_JSON_VALUE, schema = @Schema(implementation = SearchTerm.class))}),
+//            @ApiResponse(responseCode = "404", description = "Random search term was not found", content = @Content),
+//            @ApiResponse(responseCode = "400", description = "Failed to find a random search term", content = @Content) })
+//    public @NotNull ResponseEntity<SearchTerm> findRandom() {
+//        Optional<SearchTerm> item = searchTermService.findRandom();
+//        return item.map(ResponseEntity::ok).orElseGet(() -> ResponseEntity.notFound().build());
+//    }
+//
+//    @GetMapping(path = RANDOM_ID_PATH)
+//    @Timed(value = "SearchTermController.findRandomId.timer", description = "Timer for search term findRandomId endpoint", percentiles = { 0.01, 0.05,0.50, 0.95, 0.99})
+//    @Counted(value = "SearchTermController.findRandomId.counter", description = "Counter for search term findRandomId endpoint")
+//    @Operation(summary = "Find a random search term ID")
+//    @ApiResponses(value = {
+//            @ApiResponse(responseCode = "200", description = "Random search term ID successfully found", content = { @Content(mediaType = APPLICATION_JSON_VALUE, schema = @Schema(implementation = Long.class))}),
+//            @ApiResponse(responseCode = "404", description = "Random search term ID was not found", content = @Content),
+//            @ApiResponse(responseCode = "400", description = "Failed to find a random search term ID", content = @Content) })
+//    public @NotNull ResponseEntity<Long> findRandomId() {
+//        Optional<Long> item = searchTermService.findRandomId();
+//        return item.map(ResponseEntity::ok).orElseGet(() -> ResponseEntity.notFound().build());
+//    }
+//
+//    @PostMapping
+//    @Timed(value = "SearchTermController.save.timer", description = "Timer for search term save endpoint", percentiles = { 0.01, 0.05,0.50, 0.95, 0.99})
+//    @Counted(value = "SearchTermController.save.counter", description = "Counter for search term save endpoint")
+//    @Operation(summary = "Create an search term")
+//    @ApiResponses(value = {
+//            @ApiResponse(responseCode = "201", description = "Search term created successfully", content = { @Content(mediaType = APPLICATION_JSON_VALUE, schema = @Schema(implementation = SearchTerm.class))}),
+//            @ApiResponse(responseCode = "400", description = "Failed to create an search term", content = @Content) })
+//    public @NotNull ResponseEntity<SearchTerm> save(@Parameter(description = "Search term to be saved") @RequestBody @Valid @NotNull SearchTerm searchTerm) {
+//        HttpHeaders httpHeaders = new HttpHeaders();
+//        URI location = linkTo(SearchTermController.class).slash(searchTerm.getId()).toUri();
+//        httpHeaders.setLocation(location);
+//        SearchTerm saved = searchTermService.save(searchTerm);
+//        return new ResponseEntity<>(saved, httpHeaders, HttpStatus.CREATED);
+//    }
+//
+//    @DeleteMapping()
+//    @Timed(value = "SearchTermController.delete.timer", description = "Timer for search term delete endpoint", percentiles = { 0.01, 0.05,0.50, 0.95, 0.99})
+//    @Counted(value = "SearchTermController.delete.counter", description = "Counter for search term delete endpoint")
+//    @Operation(summary = "Delete a search term")
+//    @ApiResponses(value = {
+//            @ApiResponse(responseCode = "204", description = "Search term successfully deleted"),
+//            @ApiResponse(responseCode = "400", description = "Failed to delete search term", content = @Content) })
+//    public @NotNull ResponseEntity<SearchTerm> delete(@Parameter(description = "Search term to be deleted") @RequestBody @NotNull @Valid SearchTerm searchTerm) {
+//        Optional<SearchTerm> deleted = searchTermService.delete(searchTerm);
+//        return deleted.map(ResponseEntity::ok).orElseGet(() -> ResponseEntity.notFound().build());
+//    }
+//
+//    @DeleteMapping("/{id}")
+//    @Timed(value = "SearchTermController.deleteById.timer", description = "Timer for search term deleteById endpoint", percentiles = { 0.01, 0.05,0.50, 0.95, 0.99})
+//    @Counted(value = "SearchTermController.deleteById.counter", description = "Counter for search term deleteById endpoint")
+//    @Operation(summary = "Delete an search term by id")
+//    @ApiResponses(value = {
+//            @ApiResponse(responseCode = "204", description = "Search term successfully deleted by id"),
+//            @ApiResponse(responseCode = "400", description = "Failed to delete search term by id", content = @Content) })
+//    public @NotNull ResponseEntity<SearchTerm> deleteById(@Parameter(description = "The id of the search term to be deleted") @PathVariable("id") @NotNull Long id) {
+//        Optional<SearchTerm> deleted = searchTermService.deleteById(id);
+//        return deleted.map(ResponseEntity::ok).orElseGet(() -> ResponseEntity.notFound().build());
+//    }
+//
+//    @PutMapping
+//    @Timed(value = "SearchTermController.update.timer", description = "Timer for search term update endpoint", percentiles = { 0.01, 0.05,0.50, 0.95, 0.99})
+//    @Counted(value = "SearchTermController.update.counter", description = "Counter for search term update endpoint")
+//    @Operation(summary = "Update an search term")
+//    @ApiResponses(value = {
+//            @ApiResponse(responseCode = "200", description = "Search term updated successfully", content = { @Content(mediaType = APPLICATION_JSON_VALUE, schema = @Schema(implementation = SearchTerm.class))}),
+//            @ApiResponse(responseCode = "404", description = "Search term not found", content = @Content),
+//            @ApiResponse(responseCode = "400", description = "Failed to update search term", content = @Content) })
+//    public @NotNull ResponseEntity<SearchTerm> update(@Parameter(description = "Search term to be updated") @RequestBody @NotNull @Valid SearchTerm searchTerm) {
+//        Optional<SearchTerm> updated = searchTermService.update(searchTerm);
+//        return updated.map(ResponseEntity::ok).orElseGet(() -> ResponseEntity.notFound().build());
+//    }
+//
+//    @GetMapping(path = COUNT_PATH)
+//    @Timed(value = "SearchTermController.count.timer", description = "Timer for search term count endpoint", percentiles = { 0.01, 0.05,0.50, 0.95, 0.99})
+//    @Counted(value = "SearchTermController.count.counter", description = "Counter for search term count endpoint")
+//    @Operation(summary = "Return the number of existing search terms")
+//    @ApiResponses(value = {
+//            @ApiResponse(responseCode = "200", description = "Search term counted successfully", content = { @Content(mediaType = APPLICATION_JSON_VALUE, schema = @Schema(implementation = Long.class))}),
+//            @ApiResponse(responseCode = "400", description = "Failed to count search terms", content = @Content) })
+//    public @NotNull ResponseEntity<Long> count() {
+//        long count = searchTermService.count();
+//        return ResponseEntity.ok(count);
+//    }
+}
---
+++ src/test/java/org/karp/k4t/controller/UserControllerTest.java
@@ -0,0 +1,217 @@
+package org.karp.k4t.controller;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.karp.k4t.model.User;
+import org.karp.k4t.model.UserTest;
+import org.springframework.http.MediaType;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.MvcResult;
+import org.springframework.test.web.servlet.setup.MockMvcBuilders;
+import org.springframework.web.context.WebApplicationContext;
+
+import java.util.List;
+
+import static java.lang.String.format;
+import static org.hamcrest.Matchers.containsString;
+import static org.junit.jupiter.api.Assertions.assertNotNull;
+import static org.karp.k4t.integration.Paths.USER_PATH;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.log;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+class UserControllerTest extends UserTest {
+
+    private MockMvc mvc;
+
+    @BeforeEach
+    public void setup(WebApplicationContext webApplicationContext) {
+        mvc = MockMvcBuilders.webAppContextSetup(webApplicationContext).build();
+    }
+
+    @Test
+    public void shouldFindAll() throws Exception {
+        List<User> items = userContentProvider.get(getNumberOfItemsToLoad());
+        Iterable<User> saved = userRepository.saveAll(items);
+        MvcResult mvcResult = mvc.perform(get(format("/%s", USER_PATH))
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .accept(MediaType.APPLICATION_JSON))
+                .andDo(log())
+                .andExpect(status().isOk())
+                .andExpect(content().string(containsString(items.get(0).getId().toString())))
+                .andReturn();
+        assertNotNull(mvcResult);
+        userRepository.deleteAll(saved);
+    }
+
+//    @Test
+//    public void shouldFindById() throws Exception {
+//        List<SearchTerm> items = searchTermContentProvider.get(getNumberOfItemsToLoad());
+//        Iterable<SearchTerm> saved = searchTermRepository.saveAll(items);
+//        Long id = items.get(0).getId();
+//        MvcResult mvcResult = mvc.perform(get(format("/%s/{id}", SEARCH_TERM_PATH), id)
+//                        .contentType(MediaType.APPLICATION_JSON)
+//                        .accept(MediaType.APPLICATION_JSON))
+//                .andDo(log())
+//                .andExpect(status().isOk())
+//                .andExpect(content().string(containsString(id.toString())))
+//                .andReturn();
+//        assertNotNull(mvcResult);
+//        searchTermRepository.deleteAll(saved);
+//    }
+//
+//    @Test
+//    public void shouldNotFindById() throws Exception {
+//        MvcResult mvcResult = mvc.perform(get(format("/%s/{id}", SEARCH_TERM_PATH), getNonExistingId())
+//                        .contentType(MediaType.APPLICATION_JSON)
+//                        .accept(MediaType.APPLICATION_JSON))
+//                .andDo(log())
+//                .andExpect(status().isNotFound())
+//                .andReturn();
+//        assertNotNull(mvcResult);
+//    }
+//
+//    @Test
+//    public void shouldFindRandom() throws Exception {
+//        MvcResult mvcResult = mvc.perform(get(format("/%s/%s", SEARCH_TERM_PATH, RANDOM_PATH))
+//                        .contentType(MediaType.APPLICATION_JSON)
+//                        .accept(MediaType.APPLICATION_JSON))
+//                .andDo(log())
+//                .andExpect(status().isOk())
+//                .andReturn();
+//        assertNotNull(mvcResult);
+//        Integer id = JsonPath.read(mvcResult.getResponse().getContentAsString(), "$.id");
+//        assertNotNull(id);
+//    }
+//
+//    @Test
+//    public void shouldFindRandomId() throws Exception {
+//        MvcResult mvcResult = mvc.perform(get(format("/%s/%s", SEARCH_TERM_PATH, RANDOM_ID_PATH))
+//                        .contentType(MediaType.APPLICATION_JSON)
+//                        .accept(MediaType.APPLICATION_JSON))
+//                .andDo(log())
+//                .andExpect(status().isOk())
+//                .andReturn();
+//        assertNotNull(mvcResult);
+//    }
+//
+//    @Test
+//    public void shouldSave() throws Exception {
+//        SearchTerm item = searchTermContentProvider.get();
+//        MvcResult mvcResult = mvc.perform(post(format("/%s", SEARCH_TERM_PATH))
+//                        .contentType(MediaType.APPLICATION_JSON)
+//                        .content(new ObjectMapper().writeValueAsString(item))
+//                        .accept(MediaType.APPLICATION_JSON))
+//                .andDo(log())
+//                .andExpect(status().isCreated())
+//                .andReturn();
+//        assertNotNull(mvcResult);
+//        Integer id = JsonPath.read(mvcResult.getResponse().getContentAsString(), "$.id");
+//        assertNotNull(id);
+//        searchTermRepository.deleteById(id.longValue());
+//    }
+//
+//    @Test
+//    public void shouldDelete() throws Exception {
+//        SearchTerm item = searchTermContentProvider.get();
+//        SearchTerm saved = searchTermRepository.save(item);
+//        MvcResult mvcResult = mvc.perform(delete(format("/%s", SEARCH_TERM_PATH))
+//                        .contentType(MediaType.APPLICATION_JSON)
+//                        .content(new ObjectMapper().writeValueAsString(saved))
+//                        .accept(MediaType.APPLICATION_JSON))
+//                .andDo(log())
+//                .andExpect(status().isOk())
+//                .andReturn();
+//        assertNotNull(mvcResult);
+//        Integer id = JsonPath.read(mvcResult.getResponse().getContentAsString(), "$.id");
+//        assertNotNull(id);
+//    }
+//
+//    @Test
+//    public void shouldNotDelete() throws Exception {
+//        SearchTerm item = searchTermContentProvider.get();
+//        SearchTerm toBeDeleted = createSearchTermWithNonExistingId(item);
+//        MvcResult mvcResult = mvc.perform(delete(format("/%s", SEARCH_TERM_PATH))
+//                        .contentType(MediaType.APPLICATION_JSON)
+//                        .content(new ObjectMapper().writeValueAsString(toBeDeleted))
+//                        .accept(MediaType.APPLICATION_JSON))
+//                .andDo(log())
+//                .andExpect(status().isNotFound())
+//                .andReturn();
+//        assertNotNull(mvcResult);
+//    }
+//
+//    @Test
+//    public void shouldDeleteById() throws Exception {
+//        SearchTerm item = searchTermContentProvider.get();
+//        SearchTerm saved = searchTermRepository.save(item);
+//        Long id = saved.getId();
+//        MvcResult mvcResult = mvc.perform(delete(format("/%s/{id}", SEARCH_TERM_PATH), id)
+//                        .contentType(MediaType.APPLICATION_JSON)
+//                        .accept(MediaType.APPLICATION_JSON))
+//                .andDo(log())
+//                .andExpect(status().isOk())
+//                .andReturn();
+//        assertNotNull(mvcResult);
+//        boolean exists = searchTermRepository.existsById(id);
+//        assertFalse(exists);
+//    }
+//
+//    @Test
+//    public void shouldNotDeleteById() throws Exception {
+//        MvcResult mvcResult = mvc.perform(delete(format("/%s/{id}", SEARCH_TERM_PATH), getNonExistingId())
+//                        .contentType(MediaType.APPLICATION_JSON)
+//                        .accept(MediaType.APPLICATION_JSON))
+//                .andDo(log())
+//                .andExpect(status().isNotFound())
+//                .andReturn();
+//        assertNotNull(mvcResult);
+//    }
+//
+//    @Test
+//    public void shouldUpdate() throws Exception {
+//        SearchTerm item = searchTermContentProvider.get();
+//        SearchTerm saved = searchTermRepository.save(item);
+//        Long id = saved.getId();
+//        MvcResult mvcResult = mvc.perform(put(format("/%s", SEARCH_TERM_PATH))
+//                        .contentType(MediaType.APPLICATION_JSON)
+//                        .content(new ObjectMapper().writeValueAsString(item))
+//                        .accept(MediaType.APPLICATION_JSON))
+//                .andDo(log())
+//                .andExpect(status().isOk())
+//                .andReturn();
+//        assertNotNull(mvcResult);
+//        Optional<SearchTerm> updated = searchTermRepository.findById(id);
+//        assertTrue(updated.isPresent());
+//        searchTermRepository.deleteById(id);
+//    }
+//
+//    @Test
+//    public void shouldNotUpdate() throws Exception {
+//        SearchTerm item = searchTermContentProvider.get();
+//        SearchTerm toBeUpdated = createSearchTermWithNonExistingId(item);
+//        MvcResult mvcResult = mvc.perform(put(format("/%s", SEARCH_TERM_PATH))
+//                        .contentType(MediaType.APPLICATION_JSON)
+//                        .content(new ObjectMapper().writeValueAsString(toBeUpdated))
+//                        .accept(MediaType.APPLICATION_JSON))
+//                .andDo(log())
+//                .andExpect(status().isNotFound())
+//                .andReturn();
+//        assertNotNull(mvcResult);
+//    }
+//
+//    @Test
+//    public void shouldCount() throws Exception {
+//        List<SearchTerm> items = searchTermContentProvider.get(getNumberOfItemsToLoad());
+//        Iterable<SearchTerm> saved = searchTermRepository.saveAll(items);
+//        MvcResult mvcResult = mvc.perform(get(format("/%s/%s", SEARCH_TERM_PATH, COUNT_PATH))
+//                        .contentType(MediaType.APPLICATION_JSON)
+//                        .accept(MediaType.APPLICATION_JSON))
+//                .andDo(log())
+//                .andExpect(status().isOk())
+//                .andReturn();
+//        assertNotNull(mvcResult);
+//        searchTermRepository.deleteAll(saved);
+//    }
+}
