7c270b9592e9b90205601fb3ebc104d5ce5b1da3
ofer.karp <ofer.karp@gmail.com>
2022-04-28 10:47:20 +0300

    initial setup

--- src/main/java/org/karp/k4t/ui/automation/runner/destination/DefaultDestinationProvider.java
+++ src/main/java/org/karp/k4t/ui/automation/runner/destination/DefaultDestinationProvider.java
@@ -2,7 +2,9 @@ package org.karp.k4t.ui.automation.runner.destination;
 
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.karp.k4t.ui.feed.FeedView;
 import org.springframework.stereotype.Service;
+import org.springframework.web.util.UriComponents;
 
 import java.net.URI;
 
@@ -15,11 +17,19 @@ public class DefaultDestinationProvider implements DestinationProvider {
 
     @Override
     public URI getDefaultView() {
-        return null;
+        log.info("Get URI for default view started");
+        UriComponents uriComponents = uriComponentsProvider.get();
+        URI result = uriComponents.toUri();
+        log.info("Get URI for default view completed. Result is {}", result);
+        return result;
     }
 
     @Override
     public URI getFeedView() {
-        return null;
+        log.info("Get URI for feed view started");
+        UriComponents uriComponents = uriComponentsProvider.get(FeedView.ROUTE);
+        URI result = uriComponents.toUri();
+        log.info("Get URI for feed view completed. Result is {}", result);
+        return result;
     }
 }
---
+++ src/test/java/org/karp/k4t/ui/automation/runner/destination/DefaultDestinationProviderTest.java
@@ -0,0 +1,32 @@
+package org.karp.k4t.ui.automation.runner.destination;
+
+import lombok.RequiredArgsConstructor;
+import org.junit.jupiter.api.Test;
+import org.karp.k4t.Application;
+import org.karp.k4t.ui.feed.FeedView;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+
+import java.net.URI;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+@SpringBootTest(classes = Application.class)
+class DefaultDestinationProviderTest {
+
+    @Autowired
+    private DestinationProvider destinationProvider;
+
+    @Test
+    public void shouldGetDefaultView() {
+        URI uri = destinationProvider.getDefaultView();
+        assertNotNull(uri);
+    }
+
+    @Test
+    public void shouldGetFeedView() {
+        URI uri = destinationProvider.getFeedView();
+        assertNotNull(uri);
+        assertTrue(uri.getPath().contains(FeedView.ROUTE));
+    }
+}
