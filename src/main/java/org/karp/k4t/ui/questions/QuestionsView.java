f9866aaf150ed13291d7e3f16287b55c76c31c06
ofer.karp <ofer.karp@gmail.com>
2022-04-30 09:54:31 +0300

    initial setup

--- src/main/java/org/karp/k4t/ui/questions/QuestionsView.java
+++ src/main/java/org/karp/k4t/ui/questions/QuestionsView.java
@@ -2,11 +2,13 @@ package org.karp.k4t.ui.questions;
 
 import com.vaadin.flow.component.Text;
 import com.vaadin.flow.component.dependency.CssImport;
+import com.vaadin.flow.component.html.Span;
 import com.vaadin.flow.component.orderedlayout.VerticalLayout;
 import com.vaadin.flow.router.PageTitle;
 import com.vaadin.flow.router.Route;
 import org.karp.k4t.ui.home.HomeLayout;
 
+import static com.vaadin.flow.component.orderedlayout.FlexComponent.Alignment.CENTER;
 import static org.karp.k4t.ui.Styles.CSS_FILE_EXTENSION;
 import static org.karp.k4t.ui.Styles.VIEWS_FOLDER;
 import static org.karp.k4t.ui.questions.QuestionsView.FOLDER;
@@ -25,13 +27,9 @@ public class QuestionsView extends VerticalLayout {
 
     public QuestionsView() {
         addClassName(ID);
-        setSizeFull();
-        setJustifyContentMode(JustifyContentMode.CENTER);
-        setDefaultHorizontalComponentAlignment(Alignment.CENTER);
-        getStyle().set("text-align", "center");
-        getStyle().set("padding", "2em");
-
-        Text text = new Text(NAME);
+        setAlignItems(CENTER);
+        Span text = new Span(NAME);
+        text.addClassName(ID.concat("-text"));
         add(text);
     }
 }
--- src/main/java/org/karp/k4t/ui/quizzes/QuizzesView.java
+++ src/main/java/org/karp/k4t/ui/quizzes/QuizzesView.java
@@ -2,11 +2,13 @@ package org.karp.k4t.ui.quizzes;
 
 import com.vaadin.flow.component.Text;
 import com.vaadin.flow.component.dependency.CssImport;
+import com.vaadin.flow.component.html.Span;
 import com.vaadin.flow.component.orderedlayout.VerticalLayout;
 import com.vaadin.flow.router.PageTitle;
 import com.vaadin.flow.router.Route;
 import org.karp.k4t.ui.home.HomeLayout;
 
+import static com.vaadin.flow.component.orderedlayout.FlexComponent.Alignment.CENTER;
 import static org.karp.k4t.ui.Styles.CSS_FILE_EXTENSION;
 import static org.karp.k4t.ui.Styles.VIEWS_FOLDER;
 import static org.karp.k4t.ui.quizzes.QuizzesView.FOLDER;
@@ -26,13 +28,9 @@ public class QuizzesView extends VerticalLayout {
 
     public QuizzesView() {
         addClassName(ID);
-        setSizeFull();
-        setJustifyContentMode(JustifyContentMode.CENTER);
-        setDefaultHorizontalComponentAlignment(Alignment.CENTER);
-        getStyle().set("text-align", "center");
-        getStyle().set("padding", "2em");
-
-        Text text = new Text(NAME);
+        setAlignItems(CENTER);
+        Span text = new Span(NAME);
+        text.addClassName(ID.concat("-text"));
         add(text);
     }
 }
